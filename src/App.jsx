86a9972dce239a9b7112a6e03e8a4ce228c931dd
plutos28 <gamevictor28@gmail.com>
2024-05-06 23:20:06 +0300

    playing with adding music controls

--- src/App.jsx
+++ src/App.jsx
@@ -7,21 +7,25 @@ function App() {
 
   return (
     <>
-      <div>
-        {/* <img className='size-[450px] absolute inset-0 mx-auto my-auto' src={coverImage} alt="Astro World Album Cover" /> */}
-
+      <div className='blur-3xl bg-gradient-to-r from-indigo-200 to-yellow-100 w-screen h-screen'>
+      </div> 
+      <div className='mx-auto my-auto absolute inset-0'>
         <Squircle
           cornerRadius={64}
           cornerSmoothing={1}
           width={450}
           height={450}
-          className="mx-auto my-auto absolute inset-0 bg-black text-white"
+          className=" mt-40 mx-auto bg-black text-white"
         >
         <img  src={coverImage} alt="Astro World Album Cover" />
-
         </Squircle>
-
-      </div> 
+        {/* progress */}
+        <div className='mt-2'>
+          <p>1:54</p>
+          <div className='mx-auto bg-black w-[400px] h-2'></div>
+          <p>3:23</p>
+        </div>
+      </div>
     </>
   )
 }
