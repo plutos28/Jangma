ae61093b83a45d3e803ed3d70e03ed28a9dd5c8d
plutos28 <gamevictor28@gmail.com>
2024-05-06 22:37:36 +0300

    add cover and have it appear as squircle

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,26 @@
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import coverImage from './assets/cover.jpg'
 import './App.css'
+import { Squircle }
+  from "@squircle-js/react"
 
 function App() {
 
   return (
     <>
       <div>
-        <h1 className="text-red-500">Jangma</h1>
+        {/* <img className='size-[450px] absolute inset-0 mx-auto my-auto' src={coverImage} alt="Astro World Album Cover" /> */}
+
+        <Squircle
+          cornerRadius={64}
+          cornerSmoothing={1}
+          width={450}
+          height={450}
+          className="mx-auto my-auto absolute inset-0 bg-black text-white"
+        >
+        <img  src={coverImage} alt="Astro World Album Cover" />
+
+        </Squircle>
+
       </div> 
     </>
   )
