bb493388cd6c67d6831c718344de4b708c429a1f
plutos28 <gamevictor28@gmail.com>
2024-05-15 18:56:22 +0300

    testing stripe gradient using react

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,50 @@ import coverImage from './assets/cover.jpg'
 import './App.css'
 import { Squircle }
   from "@squircle-js/react"
+import * as g from './stripe-gradient.js'
+import { useRef, useState } from 'react';
+import { extractColors } from 'extract-colors'
+
+
 
 function App() {
+  const canvasRef = useRef(null);
+  const [fourColor, setFourColor] = useState(['', '', '', '']);
 
+  // Function to update an item in the list by index
+  const updateItem = (index, newValue) => {
+    setFourColor(prevList => {
+      const newList = [...prevList];
+      newList[index] = newValue;
+      return newList;
+    });
+  };
+  
+  extractColors(coverImage)
+  .then(finalColor => {
+    updateItem(0, finalColor[0].hex)
+    updateItem(1, finalColor[2].hex)
+    updateItem(2, finalColor[4].hex)
+    updateItem(3, finalColor[6].hex)
+  })
+  .catch(console.error)
+
+  if(canvasRef.current != null) {
+    new Gradient({
+      canvas: canvasRef.current,
+      colors: [fourColor[0], fourColor[1], fourColor[2], fourColor[3]]
+    });
+  } else {
+    console.log("no canvas")
+  }
+  
   return (
     <>
-      <div className='blur-3xl bg-gradient-to-r from-indigo-200 to-yellow-100 w-screen h-screen'>
-      </div> 
-      <div className='mx-auto my-auto absolute inset-0'>
+      <div className='w-screen h-screen'>
+        <div className='bg-[#ffffff2c] [box-shadow:0_4px_30px_rgba(0,_0,_0,_0.1)] backdrop-filter backdrop-blur-[25px] border-[1px] border-[solid] border-[rgba(0,0,0,0.3)]'>
+          <canvas className='w-screen h-screen' ref={canvasRef}></canvas>
+        </div>
+        <div className='mx-auto my-auto absolute inset-0'>
         <Squircle
           cornerRadius={64}
           cornerSmoothing={1}
@@ -20,11 +56,13 @@ function App() {
         <img  src={coverImage} alt="Astro World Album Cover" />
         </Squircle>
         {/* progress */}
-        <div className='mt-2'>
+        {/* <div className='mt-2'>
           <p>1:54</p>
           <div className='mx-auto bg-black w-[400px] h-2'></div>
           <p>3:23</p>
+        </div> */}
         </div>
+      <div className='blur-3xl w-screen h-screen'></div>
       </div>
     </>
   )
