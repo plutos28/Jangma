42f1660a8c166b57835fb582101ca5944cd01946
plutos28 <gamevictor28@gmail.com>
2024-05-15 19:58:57 +0300

    using radial gradient

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,7 @@ import { useRef, useState, useEffect } from 'react';
 import { extractColors } from 'extract-colors'
 
 
-
 function App() {
-  const canvasRef = useRef(null);
   const [fourColor, setFourColor] = useState(['', '', '', '']);
 
   // Function to update an item in the list by index
@@ -21,41 +19,20 @@ function App() {
     });
   };
   
- 
-  useEffect(() => {
-    extractColors(coverImage)
+
+  extractColors(coverImage)
     .then(finalColor => {
       updateItem(0, finalColor[3].hex)
       updateItem(1, finalColor[2].hex)
       updateItem(2, finalColor[1].hex)
       updateItem(3, finalColor[0].hex)
+      console.log(finalColor)
+    }).catch(console.error);
 
-      new Gradient({
-        canvas: canvasRef.current,
-        // colors: ['#249e90', '#6fccc1', '#edefda', '#d3e3a0']
-        colors: [fourColor[0], fourColor[1], fourColor[2], fourColor[3]]
-      });
-    })
-    .catch(console.error)
-  
-    
-
-    // Return a cleanup function to clean up resources when the component unmounts
-    return () => {
-      // Clean up the gradient animator
-      console.log("clean up")
-    };
-  }, []); // Empty dependency array ensures that this effect runs only once on component mount
-
-
-  
   return (
     <>
       <div className='w-screen h-screen'>
-        <div className='bg-[#ffffff2c] [box-shadow:0_4px_30px_rgba(0,_0,_0,_0.1)] backdrop-filter backdrop-blur-[25px] border-[1px] border-[solid] border-[rgba(0,0,0,0.3)]'>
-          <canvas className='w-screen h-screen' ref={canvasRef}></canvas>
-        </div>
-        <div className='mx-auto my-auto absolute inset-0'>
+        <div className='mx-auto my-auto absolute inset-0' style={{background: `radial-gradient(${fourColor[1]}, hsl(210,50%,70%) )`}}>
         <Squircle
           cornerRadius={64}
           cornerSmoothing={1}
@@ -66,7 +43,6 @@ function App() {
         <img src={coverImage} alt="Astro World Album Cover" />
         </Squircle>
         </div>
-      <div className='blur-3xl w-screen h-screen'></div>
       </div>
     </>
   )
